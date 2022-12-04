10f29e4eab11b12cb177687c20b339ad865eb168
Micke Prado <m.apg@hotmail.es>
2022-12-04 17:48:55 -0600

    creacion de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,9 +6,11 @@ import { ProductsController } from './controllers/products/products.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductsService } from './services/products/products.service';
 import { CategoriesService } from './services/categories/categories.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController, ProductsController, CategoriesController],
   providers: [AppService, ProductsService, CategoriesService],
 })

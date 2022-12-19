cbf5d15d836c0cce7ecc8e4e1ef6342f42c10317
Micke Prado <m.apg@hotmail.es>
2022-12-19 15:46:56 -0600

    se agrego variable de heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,11 @@ console.log('environments: =========== ', environments[process.env.NODE_ENV]);
       envFilePath: environments.dev,
       load: [config],
       isGlobal: true,
-      // validationSchema: Joi.object({
-      //   API_KEY: Joi.number().required(),
-      //   DATABASE_NAME: Joi.string().required(),
-      //   DATABASE_PORT: Joi.number().required(),
-      // }),
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     UsersModule,
     ProductsModule,
